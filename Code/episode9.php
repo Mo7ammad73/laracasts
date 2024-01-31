af01337451ea3a642c12402f64bf7d44fb669746
Mohammad <bakhshianmo@gmail.com>
2024-01-31 06:33:12 +0330

    assign call function to array then iterate array

---
+++ Code/episode9.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Episode9</title>
+</head>
+<body>
+<?php
+    $books = [
+        [
+            "name" => "english" ,
+            "author" => "a" ,
+            "purchaseurl" => "https://google.com"
+        ],
+        [
+            "name" => "web" ,
+            "author" => "b" ,
+            "purchaseurl" => "https://bing.com"
+        ],
+        [
+            "name" => "css" ,
+            "author" => "a" ,
+            "purchaseurl" => "https://w3school.com"
+        ]
+    ];
+    function FilterByAuthor($books , $author){
+        $filteredbook = [];
+        foreach($books as $book) {
+            if ($book['author'] == $author){
+                $filteredbook[] = $book ;
+            }
+        }
+        return $filteredbook;
+    }
+    $filteredbook=FilterByAuthor($books,'a')
+?>
+<ul>
+    <?php foreach ($filteredbook as $book) {
+        echo "<li>" . $book['name'] . "</li>";
+    }
+    ?>
+
+</ul>
+</body>
+</html>
