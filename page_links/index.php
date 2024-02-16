339bbe6035db1a3fd30e16e9697bbd41322585cd
Mohammad <bakhshianmo@gmail.com>
2024-02-16 11:28:57 +0330

    page links

---
+++ page_links/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$business = [
+    'name' => 'Laracast' ,
+    'cost' => 15 ,
+    'categories' => [ "Testing" , "PHP" , "JavaScript"]
+];
+
+function register($user) {
+
+    //create the user record in the db.
+    //log them in.
+    //send a welcome email.
+    //redirected to their new dashboard.
+
+}
+
+require "index.view.php";
